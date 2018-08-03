3192c8bbfd1b155fcaff2fcd7ef153cfcf44e147
邓勇 <dengyong@minivision.cn>
2018-08-03 11:34:06 +0800

    删除旧文件插件

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,19 +1,23 @@
 var path = require('path')
 var htmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
+  context: path.resolve(__dirname, '../'),
   entry: {
     index: "./src/index.js"
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: "[name]-[hash].js" 
+    filename: "[name].js" 
   },
   devtool: 'eval-source-map',
   plugins : [
+    new CleanWebpackPlugin(['../dist']),
     new htmlWebpackPlugin({
       filename: 'index.html',
       template: './index.html',
       inject: 'true'
-  })
+    })
+    
   ]
 } 
